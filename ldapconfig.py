579942c62a474543ae06b8087ff3b4aad53f3e69
shun <shun@isasaka.jp>
2015-09-11 17:23:54 +0900

    ldapconfig.py refactoring tu use pep3

--- ldapconfig.py
+++ ldapconfig.py
@@ -1,154 +1,131 @@
-#-*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import ldap
 import sys
-import csv
 
 from flask import Flask, render_template
 from flask import request
-from flask import redirect, url_for
+from flask import redirect
 
 app = Flask(__name__)
 
-LDAPURL='ldap://localhost/'
-ROOTDN=',dc=example,dc=com'
-USER = ''
-PASS=''
-BASE='cn=config'
-SCOPE=ldap.SCOPE_BASE
-FILTER=""
-ATTR=None
-logleveldata=[["1","trace"],["2","packets"],["4","args"],["8","conns"],["16","BER"],\
-              ["32","filter"],["64","config"],["128","ACL"],["256","stats"],\
-              ["512","stats2"],["1024","shell"],["2048","parse"]]
+LDAPURL = 'ldap://localhost/'
+DOMAIN = ',dc=example,dc=com'
+BASE = 'cn=config'
+SCOPE = ldap.SCOPE_BASE
+FILTER = ""
+ATTR = None
 PARAMETER = 'olcLogLevel'
 
-@app.route("/" , methods=['GET','POST'])
+
+@app.route("/", methods=['GET', 'POST'])
 def login():
-    print "LOGIN"
+
     err = False
-    
-    if request.method == 'POST':
-        # ユーザー名、パスワードの取得
-        USER = request.form.get('user')
-        PASS = request.form.get('pass')
-        
-        # 乳力された文字列を結合してROOTDNの作成
-        ROOT = "cn=" + USER + ROOTDN
 
-        # 入力されたデータでldapサーバーへ接続
-        ld = ldapconnect(LDAPURL,ROOT,PASS)
-        
-        # 認証に失敗した場合エラーを出力
-        if ld == -1:
+    if request.method == 'POST':
+        user = request.form.get('user')
+        password = request.form.get('pass')
+
+        ROOT = "cn=" + user + DOMAIN
+
+        ld = ldapconnect(LDAPURL, ROOT, password)
+
+        if not ld:
             err = True
-            print "Err"
-            return render_template('loginform.html',err = err,)
-        # 成功した場合ldapConfigへ移動
-        else :
-            print "Success"
-            return redirect('http://localhost:5000/ldapconfig')
+            return render_template('loginform.html', err=err,)
+        else:
+            return redirect('/ldapconfig')
+
     if request.method == 'GET':
-        return render_template('loginform.html',err = False,)
-       
+        return render_template('loginform.html', err=False,)
 
-@app.route("/ldapconfig" , methods=['GET' , 'POST'])
-def index() :
+
+@app.route("/ldapconfig", methods=['GET', 'POST'])
+def index():
+    logleveldata = [["1", "trace"], ["2", "packets"], ["4", "args"],
+                    ["8", "conns"], ["16", "BER"], ["32", "filter"],
+                    ["64", "config"], ["128", "ACL"], ["256", "stats"],
+                    ["512", "stats2"], ["1024", "shell"], ["2048", "parse"]]
+
     POSTDATA = ""
-    errlist=[]
 
-    #####################
-    # ldapサーバーへ接続#
-    #####################
-    ROOT = "cn=admin" + ROOTDN.decode('utf-8')
+    ROOT = "cn=admin" + DOMAIN.decode('utf-8')
     PASS = "password"
-    ld = ldapconnect(LDAPURL,ROOT,PASS)
+    ld = ldapconnect(LDAPURL, ROOT, PASS)
 
-    #############
-    # POSTの処理#
-    #############
     if request.method == 'POST':
-        # POSTされたデータの取得
-        CHECK = request.form.keys()
-        # チェックされた値のリストを作成
-        CHECK.remove('button')
+        check = request.form.keys()
+        # チェックボックスの状態を取得するため
+        # すべてのkeyを取得し、buttonのデータを削除
+        check.remove('button')
 
-        print CHECK
-        # チェックボックスが空ならnoneを指定 
-        if len(CHECK) == 0:
-            CHECK.append('none')
-        
-        # CHECKの値でmodify
-        ldapmodify(ld,CHECK)
-                               
-    ############
-    # GETの処理#
-    ############
+        if len(check) == 0:
+            check.append('none')
 
-    ###############
-    # ページの表示#
-    ############### 
-    # データの取得
-    loglevel = ldapsearch(ld,BASE,SCOPE,PARAMETER)
+        ldapmodify(ld, check)
 
-    # logleveldataと比較し、TrueFalseの一覧リストを作成
-    for loop in range(0,len(logleveldata)):
+    loglevel = ldapsearch(ld, BASE, SCOPE, PARAMETER)
+
+    for loop in range(0, len(logleveldata)):
         if len(logleveldata[len(logleveldata)-1]) < 3:
             logleveldata[loop].append(logleveldata[loop][1] in loglevel)
-        else :
+        else:
             logleveldata[loop][2] = (logleveldata[loop][1] in loglevel)
-    # ページヘの出力
-    return render_template('ldapconfig.html',loglevels=logleveldata, errlist=errlist)
+
+    return render_template('ldapconfig.html', loglevels=logleveldata)
 
 
+'''
+ldapに接続する関数
+'''
 
 
-####################
-#ldapに接続する関数#
-####################
-def ldapconnect(ldapurl,rootdn,password):
-    try :
-        ld=ldap.initialize(ldapurl)
-        ld.simple_bind_s(rootdn,password)
+def ldapconnect(ldapurl, rootdn, password):
+    try:
+        ld = ldap.initialize(ldapurl)
+        ld.simple_bind_s(rootdn, password)
     except:
-        return -1
-  
+        print sys.exc_info()
+        return False
+
     return ld
 
-####################
-#ldapmodifyする関数#
-####################
-def ldapmodify(ld,datas):
-    # modlistの作成
-    modlist=[]
+'''
+ldapmodifyする関数
+'''
+
+
+def ldapmodify(ld, datas):
+    modlist = []
     for data in datas:
         if len(modlist) == 0:
-            modlist.append( (ldap.MOD_REPLACE,PARAMETER,data) )
+            modlist.append((ldap.MOD_REPLACE, PARAMETER, data))
         else:
-            modlist.append( (ldap.MOD_ADD,PARAMETER,data) )
+            modlist.append((ldap.MOD_ADD, PARAMETER, data))
 
-    # 作成したmodデータを用いてModify
-    try :
-        ld.modify_ext_s(BASE,modlist)
-    except:
-        return "<h1> ModifyErr! </h1><p>{err}</p>".format(err=sys.exc_info())
-
-
-###############################
-#PARAMETERの値をsearchする関数#
-###############################
-def ldapsearch(ld,base,scope,parameter):
-    # 全属性の取得
     try:
-        search_results = ld.search_ext_s(BASE,SCOPE)
+        ld.modify_ext_s(BASE, modlist)
     except:
-        return "<h1> SearchErr! </h1> <p>{err}</p>".format(err=sys.exc_info())
+        print sys.exc_info()
+        return False
 
-    # PARAMETERの値を取得
-    datalist = search_results[0][1].get(PARAMETER,[])
+'''
+PARAMETERの値をsearchする関数
+'''
+
+
+def ldapsearch(ld, base, scope, parameter):
+    try:
+        search_results = ld.search_ext_s(BASE, SCOPE)
+    except:
+        print sys.exc_info()
+        return False
+
+    datalist = search_results[0][1].get(PARAMETER, [])
 
     return datalist
 
 
-if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
